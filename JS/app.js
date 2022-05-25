17266539f2d646ad2424a403c7ef2ef154af2a7d
PoshoFB <arqfernandezblanco@gmail.com>
2022-05-25 16:44:30 -0300

    Se alinearon las imagenes, margenes, paddings. Se ajusto QUIENES SOMOS

--- JS/app.js
+++ JS/app.js
@@ -29,3 +29,15 @@ form.addEventListener("submit", (e) => {
     parrafo.innerHTML = "Muchas gracias. Nos contactaremos a la brevedad";
   }
 });
+
+// Menu Hamburguesa
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
